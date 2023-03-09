6c1a23498adb4bea2bc7dfa9356e33c8726619f4
tobiasekholt <tobias.ekholt16@gmail.com>
2023-03-09 08:47:33 +0100

    test

--- ngisopenapi/demo.py
+++ ngisopenapi/demo.py
@@ -6,6 +6,7 @@ import os
 import json 
 import uuid
 import transfer
+import bbox_extractor
 
 from api import NgisOpenApi
 
@@ -34,12 +35,15 @@ def main() -> int:
     #print(api.get_dataset_info(dataset_id))
     
     print("Get features")
-    bbox = "584501.1079749034,6639556.316979835,585975.5127302224,6640769.565718482"
+    bbox = "584080.3856561417,6638847.17958132,584237.6979578076,6639009.613057086"
+    bbox_geojson = bbox_extractor.get_bbox()
     bbox_dataset = "229000,759000,6398000,7265000"
-    filename = str(uuid.uuid4()) + ".geojson"
-    res = api.get_features(dataset_id, bbox)
     
+    filename = str(uuid.uuid4()) + ".geojson"
+    
+    res = api.get_features(dataset_id, bbox, "Bygning")
     print(f'Got {len(res["features"])} features. Saving to {filename}')
+    
     '''
     for f in res["features"]:
         print(f["properties"]["featuretype"])
