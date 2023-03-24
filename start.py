acd12fb55681c7845d7e6b9f41f8e57701c083b6
tobiasekholt <tobias.ekholt16@gmail.com>
2023-03-24 13:29:36 +0100

    Start.py og Dockerfil er lagt til

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 import subprocess
 
-subprocess.call(['cmd.exe', '/c', 'cd ngisopenapi && conda activate venv && python demo.py && conda deactivate && cd ..'])
-subprocess.call(['cmd.exe', '/c', 'cd kartAI && conda activate gdal_env && kai.bat create_training_data -n small_test_area -c config/dataset/kartai.json --region training_data/regions/small_building_region.json && conda deactivate && cd ..'])
-subprocess.call(['cmd.exe', '/c', 'cd kartAI && conda activate gdal_env && kai.bat train -dn small_test_area -m unet -cn test_small_area_unet -c config/ml_input_generator/ortofoto.json'])
-
+subprocess.call(['sh', '-c', 'cd ngisopenapi && python demo.py && cd ..'])
+subprocess.call(['sh', '-c', 'cd kartAI && ./kai.sh create_training_data -n small_test_area -c config/dataset/kartai.json --region training_data/regions/small_building_region.json && cd ..'])
+subprocess.call(['sh', '-c', 'cd kartAI && ./kai.sh train -dn small_test_area -m unet -cn test_small_area_unet -c config/ml_input_generator/ortofoto.json'])
