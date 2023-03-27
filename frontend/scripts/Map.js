e5d7d69a40bc567234f4ffb4b78ae17b5a137567
LudaKasp <kapny2009@hotmail.no>
2023-03-27 12:08:53 +0200

    fjernet ting som ikke trengte være i koden

--- frontend/scripts/Map.js
+++ frontend/scripts/Map.js
@@ -74,7 +74,9 @@ var uniqueCoords = uniqueCoordsArray.map(function(coord) {
 var coordinatesElement = document.getElementById("coordinates");
 var coordinatesString = "";
 for (var i = 0; i < uniqueCoords.length; i++) {
-    coordinatesString += "<b>P" + (i+1) + ": </b>" + uniqueCoords[i].lat + ", " + uniqueCoords[i].lng + "<br>";
+    // Convert the LatLng to EPSG:3857 Point
+    var point = L.CRS.EPSG3857.project(uniqueCoords[i]);
+    coordinatesString += "<b>P" + (i+1) + ": </b>" + point.x + ", " + point.y + "<br>";
 }
 
 coordinatesElement.innerHTML = coordinatesString;
