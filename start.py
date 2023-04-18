40339edd067f422124d7a4d19d12b74937e4368d
niklasfugledal <niklasfugledal@live.no>
2023-04-18 09:41:58 +0200

    fungerende endringer

--- start.py
+++ start.py
@@ -1,26 +1,8 @@
 # start.py code
+# Check correct pathing for you computer to where anaconda is installed and all the way to the activate.
 
 import subprocess
 
 print('Starting training process...')
-subprocess.call(['cmd.exe', '/c', 'cd ngisopenapi && conda activate venv && python demo.py && conda deactivate && cd ..'])
-subprocess.call(['cmd.exe', '/c', 'cd kartAI && conda activate gdal_env && kai.bat create_training_data -n small_test_area -c config/dataset/kartai.json --region training_data/regions/small_building_region.json && conda deactivate && cd ..'])
-subprocess.call(['cmd.exe', '/c', 'cd kartAI && conda activate gdal_env && kai.bat train -dn small_test_area -m unet -cn test_small_area_unet -c config/ml_input_generator/ortofoto.json'])
+subprocess.call('start cmd.exe /k "%USERPROFILE%\\anaconda3\\Scripts\\activate && run_all_commands.bat"', shell=True)
 
-
-
-
-
-#Teste dette 
-
-# get the absolute path to the directory containing start.py
-#root_dir = Path(__file__).resolve().parent.parent
-
-# construct the paths to the ngisopenapi and kartAI directories
-#ngisopenapi_dir = root_dir / 'ngisopenapi'
-#kartAI_dir = root_dir / 'kartAI'
-
-# execute the commands
-#subprocess.call(['sh', '-c', 'cd ngisopenapi && python demo.py && cd ..'])
-#subprocess.call(['sh', '-c', 'cd kartAI && ./kai create_training_data -n small_test_area -c config/dataset/kartai.json --region training_data/regions/small_building_region.json && cd ..'])
-#subprocess.call(['sh', '-c', 'cd kartAI && ./kai train -dn small_test_area -m unet -cn test_small_area_unet -c config/ml_input_generator/ortofoto.json'])
