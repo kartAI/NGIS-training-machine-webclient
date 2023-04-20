66cdb359112ce0dee1fd1d576898c81d8fae8a11
tobiasekholt <tobias.ekholt16@gmail.com>
2023-04-20 09:29:14 +0200

    Lagt til funksjon for sletting av db data

--- start.py
+++ start.py
@@ -2,11 +2,10 @@
 
 import subprocess
 
-print('Starting training process...')
+print('Starting download process...')
 subprocess.call(['cmd.exe', '/c', 'cd ngisopenapi && conda activate venv && python demo.py && conda deactivate && cd ..'])
 subprocess.call(['cmd.exe', '/c', 'cd kartAI && conda activate gdal_env && kai.bat create_training_data -n small_test_area -c config/dataset/kartai.json --region training_data/regions/small_building_region.json -eager True && conda deactivate && cd ..'])
-#subprocess.call(['cmd.exe', '/c', 'cd kartAI && conda activate gdal_env && kai.bat train -dn small_test_area -m unet -cn test_small_area_unet -c config/ml_input_generator/ortofoto.json'])
-
+subprocess.call(['cmd.exe', '/c', 'cd ngisopenapi && conda activate venv && python deleteDB.py && conda deactivate && cd ..'])
 
 
 
