d796b076c3f4d2def0d56b89dbb710e9c31a9d41
LudaKasp <kapny2009@hotmail.no>
2023-04-24 08:49:11 +0200

    Funksjonalitet validation/test

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,10 +1,8 @@
-
 let scrollHeight = Math.max(
   document.body.scrollHeight, document.documentElement.scrollHeight,
   document.body.offsetHeight, document.documentElement.offsetHeight,
   document.body.clientHeight, document.documentElement.clientHeight
 );
-
 
 function nav() {
   const url = window.location.href;
@@ -15,8 +13,38 @@ function nav() {
   }
 }
 
+async function updateValue() {
+  let inputTraining = document.getElementById("training");
+  let inputValidation = document.getElementById("validation");
+  let training_fraction = [inputTraining.value, inputValidation.value];
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  console.log(training_fraction);
+
+    const response = await fetch('http://localhost:8000/update_training', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(training_fraction),
+    });
+  
+    const data = await response.json();
+    return data;
+}
+
+function startTraining() {
+  // Show the modal
+  $('#confirmationModal').modal('show');
+}
+
+function confirmTraining() {
+  fetch('/startTraining', {
+      method: 'POST'
+  })
+      .then(() => {
+          // Navigate to confirm.html regardless of the response
+          window.location.href = '/sendToEmail.html';
+      })
+      .catch(error => console.error(error));
+}
+
