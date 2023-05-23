e4d6f83211fc2cdeeb4033fbc7df40d407e493f6
LudaKasp <kapny2009@hotmail.no>
2023-05-23 10:55:03 +0200

    formatering og små endringer i readme

--- deleteFolder.py
+++ deleteFolder.py
@@ -2,12 +2,15 @@ import os
 import shutil
 
 # Define function to delete a folder
+
+
 def delete_folder(folder_path):
     if os.path.exists(folder_path):
         shutil.rmtree(folder_path)  # Delete folder and its contents
         print(f'Deleted folder: {folder_path}')
     else:
         print(f'The folder does not exist: {folder_path}')
+
 
 # Get base directory of script file
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,6 +23,8 @@ folder_2 = os.path.join(
 folder_3 = os.path.join(BASE_DIR, "kartAI", "training_data", "OrtofotoWMS")
 
 # Define main function to delete folders and their contents
+
+
 def main():
     print("Starting deletion of folders...")
     delete_folder(training_data_folder)  # Delete first folder
@@ -27,11 +32,14 @@ def main():
     delete_folder(folder_3)  # Delete third folder
     print("Folders were deleted successfully.")
 
+
 # Check if script is being run directly (as opposed to being imported as a module)
 if __name__ == "__main__":
     main()  # Call main function
 
 # Define a second function that does the same thing as main
+
+
 def delete_all_folders():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     training_data_folder = os.path.join(
