163987a7395b8c9e7967c3424bae60403204e73b
Gustav Svartsund <gustavsv@uia.no>
2024-02-05 13:10:36 +0100

    test

--- frontend/scripts/map.js
+++ frontend/scripts/map.js
@@ -1,6 +1,6 @@
 // Map initialization
-var map = L.map('map', { crs: L.CRS.EPSG3857 }).setView([59.887537, 10.523083], 14);
-
+var map = L.map('map', { crs: L.CRS.EPSG3857 }).setView([59.917107, 10.727496], 10);
+ 
 // Add OpenStreetMap Tiles
 var osm = L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=gBHYqk3cSCXUdQqICyH3', {
     attribution: '<a href=" https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a><a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
