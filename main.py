7a2176fd7ab51eceb54d22af7b3c25c370781c63
Cassandra <cassvaar@gmail.com>
2024-02-16 12:30:04 +0100

    her er greiene victor

--- main.py
+++ main.py
@@ -308,3 +308,32 @@ async def update_wms_config_file(configInput: ConfigInput):
     except Exception as e:
       raise HTTPException(status_code=500, detail=f"Failed to write config to json file: {str(e)}")
     return {"Message": "Config was updated successfully"}
+
+
+@app.get("/generatePhotos")
+async def generatePhotos():
+    #Read the coordinates from the file
+    file = open(COORDINATE_FILE)
+    data = json.load(file)
+    coordinates = data["Coordinates"];
+
+    #Read config from the file
+    file = open(CONFIG_FILE)
+    data = json.load(file)
+    config = data["Config"];
+
+    generate_wms_picture(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+    generate_wms_photo(coordinates, config)
+
+@app.get("/getAndZipWMSFiles")
+async def get_and_zip_  files():
+    folder_path = os.path.join(BASE_DIR, "kartAI", "training_data",
+                               "Training_data", "3857_563000.0_6623000.0_100.0_100.0", "512")
+    files = [f for f in os.listdir(
+        folder_path) if f.endswith(('.tif', '.json', '.vrt'))]
+    num_files = len(files)
+    if num_files == 0:
+        folder_summary = "No files found!"
+    else:
+        folder_summary = f"{num_files} file(s) selected: <br><br> {', '.join(files)}"
+    return {"folder_summary": folder_summary}
