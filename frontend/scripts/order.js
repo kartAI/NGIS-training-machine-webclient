bdf532fac76db9012f8fdf45afd90d8a533fde8b
Tørres Båshus <torresb16@uia.no>
2024-03-18 08:53:34 +0100

    Frontend endringer

---
+++ frontend/scripts/order.js
@@ -0,0 +1,29 @@
+
+//Javascript code for downloading file. missings paths and file
+document.getElementById("downloadButton").addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/path-to-your-file", true); // Specify the file path here
+    xhr.responseType = "blob";
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var blob = xhr.response;
+            var link = document.createElement("a");
+            link.href = window.URL.createObjectURL(blob);
+            link.download = "filename.extension"; // Specify the filename and extension
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    };
+    xhr.send();
+});
+
+//Fetch header
+<script>
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the header:', error));
+</script>
