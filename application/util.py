f6f3fe12540927d93dcc44e4ba6f68f7a7119267
Cassandra <cassvaar@gmail.com>
2024-04-16 10:01:05 +0200

    lagt til flere tester

--- application/util.py
+++ application/util.py
@@ -8,7 +8,7 @@ from shapely.geometry import Polygon, MultiPoint
 import shapely 
 
 
-def split_files(image_path, output_folder, tiles, training_fraction, validation_fraction):
+def split_files(image_path, output_folder, training_fraction):
     '''
     Splits a set of tiles and sorts them according to machine learning specifications
     
@@ -22,12 +22,17 @@ def split_files(image_path, output_folder, tiles, training_fraction, validation_
     Returns:
     bool: True if all files were moved successfully, False otherwise
     '''
-    # Calculate the amount of files for each fraction
-    training_files = int(training_fraction)/100 * int(tiles)
-    validation_files = int(validation_fraction)/100 * int(tiles)
 
+    if(not os.path.exists(os.path.join(image_path, "orto")) or not os.path.exists(os.path.join(image_path, "fasit")) or not os.path.exists(output_folder)):
+        return False
+    
     training_tiles = os.listdir(os.path.join(image_path, "orto"))
     validation_tiles = os.listdir(os.path.join(image_path, "fasit"))
+
+    # Calculate the amount of files for each fraction
+    total_tiles = len(training_tiles) + len(validation_tiles)
+    training_files = int(training_fraction)/100 * total_tiles  
+    
     training_tiles_moved = 0
     validation_tiles_moved = 0
 
@@ -71,7 +76,7 @@ def setup_user_session_folders(session_id):
     # If the datasets folder does not exist, create a new one
     if not os.path.exists("datasets"):
         os.mkdir("datasets")
-    # Setup the main folder 
+    # Setup the main folder
     main_folder_name = "dataset_" + str(session_id)
     os.makedirs(os.path.join("datasets", main_folder_name), exist_ok=True)
     # Setup the files for coordinates and config for each user session
@@ -123,8 +128,14 @@ def read_file(file_path):
     Args:
     file_path (str): The path to the file that you want to read
     '''
-    file = open(file_path)
-    return json.load(file)
+    if(os.path.exists(file_path)):
+        file = open(file_path)
+        data = json.load(file)
+        file.close()
+        return data
+    else:
+        raise HTTPException(status_code=500, detail=f"Could not find file: {str(file_path)}")
+        
 
 def write_file(file_path, data):
     '''
@@ -138,87 +149,56 @@ def write_file(file_path, data):
             json.dump(data, file)
             return 1
     except Exception as e:
-      raise HTTPException(status_code=500, detail=f"Failed to write config to json file: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to write config to json file: {str(e)}")
     
 def create_bbox_array(coordinates, config):
-
     '''
     Creates a bbox based on coordiantess
     Args:
     coordinates (array): An array of the coordinates you want to convert to bbox
     config (dict): Configuration settings including tile size and image resolution.
     Return:
-    The requested bbox 
+    The requested bbox or if there are not enough coordinates it returns false
     '''
-    if(len(coordinates) > 5):
-        #Create one big bbox that covers the entire polygon
-        polygon = Polygon(coordinates)
-        bigBox = polygon.envelope
-        bigBoxCoords = list(bigBox.exterior.coords)
 
-        # Calculate bounds for the bounding boxes
-        min_x = min(coord[0] for coord in bigBoxCoords)
-        min_y = min(coord[1] for coord in bigBoxCoords)
-        max_x = max(coord[0] for coord in bigBoxCoords)
-        max_y = max(coord[1] for coord in bigBoxCoords)
+    if(len(coordinates) < 4):
+        return False
 
-        # Define starting and ending points
-        starting_point = [min_x, min_y]
-        ending_point = [max_x, max_y]
-
-        # Configure tile size and resolution
-        preferred_image_size = [config["tile_size"], config["tile_size"]] # Pixels per time, recommendend is 500
-        resolution = config["image_resolution"] # Meters per pixel, recommended is 0.2
-        bbox_size = [preferred_image_size[0]*resolution, preferred_image_size[1]*resolution]
-        
-        # Determine the number of images required to cover the area
-        num_images_x = int((ending_point[0] - starting_point[0]) / bbox_size[0])
-        num_images_y = int((ending_point[1] - starting_point[1]) / bbox_size[1])
-        
-        # Generate bounding boxes
-        bboxes = []
-        for x in range(num_images_x):
-            for y in range(num_images_y):
-                x0 = starting_point[0] + (x * bbox_size[0])
-                y0 = starting_point[1] + (y * bbox_size[1])
-                x1 = starting_point[0] + ((x + 1) * bbox_size[0])
-                y1 = starting_point[1] + ((y + 1) * bbox_size[1])
-                
-                coords = [[x0, y0], [x0 + bbox_size[0], y0],[x0, y0 + bbox_size[1]], [x1, y1]]
-                bbox = Polygon(coords)
-                if(bbox.intersects(polygon)):
-                    bboxes.append([x0, y0, x1, y1])
-        return bboxes
-    else:
-        # Calculate bounds for the bounding boxes
-        min_x = min(coord[0] for coord in coordinates)
-        min_y = min(coord[1] for coord in coordinates)
-        max_x = max(coord[0] for coord in coordinates)
-        max_y = max(coord[1] for coord in coordinates)
-
-        # Define starting and ending points
-        starting_point = [min_x, min_y]
-        ending_point = [max_x, max_y]
-
-        # Configure tile size and resolution
-        preferred_image_size = [config["tile_size"], config["tile_size"]] # Pixels per time, recommendend is 500
-        resolution = config["image_resolution"] # Meters per pixel, recommended is 0.2
-        bbox_size = [preferred_image_size[0]*resolution, preferred_image_size[1]*resolution]
-        
-        # Determine the number of images required to cover the area
-        num_images_x = int((ending_point[0] - starting_point[0]) / bbox_size[0])
-        num_images_y = int((ending_point[1] - starting_point[1]) / bbox_size[1])
-        
-        # Generate bounding boxes
-        bboxes = []
-        for x in range(num_images_x):
-            for y in range(num_images_y):
-                x0 = starting_point[0] + (x * bbox_size[0])
-                y0 = starting_point[1] + (y * bbox_size[1])
-                x1 = starting_point[0] + ((x + 1) * bbox_size[0])
-                y1 = starting_point[1] + ((y + 1) * bbox_size[1])
+    #Create one big bbox that covers the entire polygon
+    polygon = Polygon(coordinates)
+    bigBox = polygon.envelope
+    bigBoxCoords = list(bigBox.exterior.coords)
+    # Calculate bounds for the bounding boxes
+    min_x = min(coord[0] for coord in bigBoxCoords)
+    min_y = min(coord[1] for coord in bigBoxCoords)
+    max_x = max(coord[0] for coord in bigBoxCoords)
+    max_y = max(coord[1] for coord in bigBoxCoords)
+    # Define starting and ending points
+    starting_point = [min_x, min_y]
+    ending_point = [max_x, max_y]
+    # Configure tile size and resolution
+    preferred_image_size = [config["tile_size"], config["tile_size"]] # Pixels per time, recommendend is 500
+    resolution = config["image_resolution"] # Meters per pixel, recommended is 0.2
+    bbox_size = [preferred_image_size[0]*resolution, preferred_image_size[1]*resolution]
+    
+    # Determine the number of images required to cover the area
+    num_images_x = int((ending_point[0] - starting_point[0]) / bbox_size[0])
+    num_images_y = int((ending_point[1] - starting_point[1]) / bbox_size[1])
+    
+    # Generate bounding boxes
+    bboxes = []
+    for x in range(num_images_x):
+        for y in range(num_images_y):
+            x0 = starting_point[0] + (x * bbox_size[0])
+            y0 = starting_point[1] + (y * bbox_size[1])
+            x1 = starting_point[0] + ((x + 1) * bbox_size[0])
+            y1 = starting_point[1] + ((y + 1) * bbox_size[1])
+            
+            coords = [[x0, y0], [x0 + bbox_size[0], y0],[x0, y0 + bbox_size[1]], [x1, y1]]
+            bbox = Polygon(coords)
+            if(bbox.intersects(polygon)):
                 bboxes.append([x0, y0, x1, y1])
-        return bboxes
+    return bboxes
 
 def create_bbox(coordinates):
     '''
