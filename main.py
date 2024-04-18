d910dc87332b10cc3302ae0b04d70653c9f8e039
Cassandra <cassvaar@gmail.com>
2024-04-18 14:21:17 +0200

    nå er det zip fil

--- main.py
+++ main.py
@@ -478,7 +478,7 @@ def send_email(to_email, attachment_name, dataset_name):
     part = MIMEBase("application", "octet_stream")
     part.set_payload((attachment).read())
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= %s" % f"{dataset_name}")
+    part.add_header('Content-Disposition', "attachment; filename= %s" % f"{dataset_name}.zip")
 
     message.attach(part)
 
