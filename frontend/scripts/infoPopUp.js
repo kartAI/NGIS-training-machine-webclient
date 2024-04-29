177d6b9da2898fc3fd1f2366d69f89d07f8fea2f
Cassandra <cassvaar@gmail.com>
2024-04-29 11:09:25 +0200

    Fikset resolution desc og oblig felter

--- frontend/scripts/infoPopUp.js
+++
@@ -1,6 +0,0 @@
-if (window.location.href.includes("methods.html")) {
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("HELLO")
-    document.getElementById("info-modal-text").innerHTML = "none";
-  });
-}
